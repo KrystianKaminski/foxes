211f959960de1150eda997ab466fee0a0b62e64b
KrystianKaminski <krystiankaminski07@gmail.com>
2020-05-19 18:56:24 +0200

    Add some transitions to elements

--- index.js
+++ index.js
@@ -1,74 +1,72 @@
 onInit = () => {
-    const current = getCurrentSlide()
-    current.classList.add('active');
+  const current = getCurrentSlide();
+  current.classList.add("active");
 
-    setUpEventListeners();
-}
+  setUpEventListeners();
+};
 
-getCurrentSlide = () => document.getElementsByClassName('form-view')[currentSlide];
-getCurrentDot = () => document.getElementsByClassName('step-line__dot')[currentSlide];
-
+getCurrentSlide = () =>
+  document.getElementsByClassName("form-view")[currentSlide];
+getCurrentDot = () =>
+  document.getElementsByClassName("step-line__dot")[currentSlide];
 
 moveToNextSlide = () => {
+  //need to be changed to slides.length later
 
-    //need to be changed to slides.length later
+  if (currentSlide <= 5) {
+    current = getCurrentSlide();
+    currentDot = getCurrentDot();
 
-    if (currentSlide <= 5) {
-        current = getCurrentSlide();
-        currentDot = getCurrentDot();
+    current.classList.remove("active");
+    currentDot.classList.add("step-line__dot--done");
 
-        current.classList.remove('active');
-        currentDot.classList.add('step-line__dot--done');
+    currentSlide++;
 
-        currentSlide++;
+    next = getCurrentSlide();
+    nextDot = getCurrentDot();
 
-        next = getCurrentSlide();
-        nextDot = getCurrentDot();
-
-        next.classList.add('active');
-        nextDot.classList.add('step-line__dot--active');
-
-    } else {
-        console.log('Its last slide')
-    }
-}
+    next.classList.add("active");
+    nextDot.classList.add("step-line__dot--active");
+  } else {
+    console.log("Its last slide");
+  }
+};
 
 moveToPreviousSlide = () => {
-    if (currentSlide > 0) {
-        current = getCurrentSlide();
-        currentDot = getCurrentDot();
+  if (currentSlide > 0) {
+    current = getCurrentSlide();
+    currentDot = getCurrentDot();
 
-        current.classList.remove('active');
-        currentDot.classList.remove('step-line__dot--active');
+    current.classList.remove("active");
+    currentDot.classList.remove("step-line__dot--active");
 
-        currentSlide--;
+    currentSlide--;
 
-        previous = getCurrentSlide();
-        previousDot = getCurrentDot();
+    previous = getCurrentSlide();
+    previousDot = getCurrentDot();
 
-        previous.classList.add('active');
-        previousDot.classList.remove('step-line__dot--done');
-        previousDot.classList.add('step-line__dot--active');
-    } else {
-        console.log('Its first slide')
-    }
-}
+    previous.classList.add("active");
+    previousDot.classList.remove("step-line__dot--done");
+    previousDot.classList.add("step-line__dot--active");
+  } else {
+    console.log("Its first slide");
+  }
+};
 
 setUpEventListeners = () => {
+  document.querySelector(".cta__next").addEventListener("click", function () {
+    console.log("next");
+    moveToNextSlide();
+  });
 
-    document.querySelector('.cta__next').addEventListener('click', function () {
-        console.log('next')
-        moveToNextSlide()
-    })
+  document.querySelector(".cta__back").addEventListener("click", function () {
+    console.log("back");
+    moveToPreviousSlide();
+  });
 
-    document.querySelector('.cta__back').addEventListener('click', function () {
-        console.log('back')
-        moveToPreviousSlide()
-    })
-
-    document.querySelector('.text-input').addEventListener('keyup', function (event) {
-        values.name = event.target.value;
-    })
-
-
-}
+  document
+    .querySelector(".text-input")
+    .addEventListener("keyup", function (event) {
+      values.name = event.target.value;
+    });
+};
